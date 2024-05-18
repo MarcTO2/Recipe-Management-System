47960fc27feb83fb46606250267b8094afbcac5c
Absalom Marcel <mrclabsalom@gmail.com>
2024-05-18 12:05:34 +0300

    urls updated

---
+++ RecipeManagment/recipes/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import RecipeListView, RecipeDetailView, RecipeCreateView, RecipeUpdateView, RecipeDeleteView
+
+urlpatterns = [
+    path('', RecipeListView.as_view(), name='recipe_list'),
+    path('recipe/<int:pk>/', RecipeDetailView.as_view(), name='recipe_detail'),
+    path('recipe/add/', RecipeCreateView.as_view(), name='recipe_add'),
+    path('recipe/<int:pk>/edit/', RecipeUpdateView.as_view(), name='recipe_edit'),
+    path('recipe/<int:pk>/delete/', RecipeDeleteView.as_view(), name='recipe_delete'),
+]
